b3e7d04c5f85a3d8678d34d232ddab5d7990353f
kooskoos20 <omkarbirade20@gmail.com>
2022-01-28 19:14:49 +0530

    feat: add Azure AD provider

--- main.ts
+++ main.ts
@@ -4,7 +4,11 @@ import { App, TerraformStack } from "cdktf";
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
-
+    new AzureadProvider(this, "azuread", {
+      tenantId: process.env.ARM_TENANT_ID,
+      clientId: process.env.ARM_CLIENT_ID,
+      clientSecret: process.env.ARM_CLIENT_SECRET
+    });
     // define resources here
   }
 }
